be7e8d47807adf3b2ea7aa6802297f5f73be7a0d
LydiaDabbah <lydiadabbah@gmail.com>
2022-10-05 23:35:38 -0500

    ready to deploy

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ app.use(express.json()) // Permitimos comunicacion a traves de formato JSON
 app.use(express.urlencoded({extended: false})) // Permitimos recibir datos de codificaciond de formularios
 
 // Utilizamos las rutas
+app.use(cors())
 app.use(router)
 
 // Levantamos el servidor con el metodo listen de express
